aa54adf12fb3b5311c4254797effde42daac73a3
Andrew <andrey.kozak@altarix.com>
2014-05-30 18:48:42 +0400

    InitialCommit

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'content_type': 'text/plain' });
+    res.end("Hello World");
+   
+}).listen(process.env.port||8080);
